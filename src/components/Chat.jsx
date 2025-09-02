7560c6a3e564280f1dbe2b161d26115bf1bec534
Edgar <ceggarr199@gmail.com>
2025-09-02 01:58:04 -0600

    correccion de ip

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -26,7 +26,7 @@ const Chat = ({ bot }) => {
     setInput("");
 
     try {
-      const response = await fetch("http://72.60.31.189:80/chat", {
+      const response = await fetch("https://72.60.31.189/chat", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({
