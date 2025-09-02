a3ca6004d6802805ba9cf58f7da71485c6e39b27
Edgar <ceggarr199@gmail.com>
2025-09-02 00:45:24 -0600

    ip

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -26,7 +26,7 @@ const Chat = ({ bot }) => {
     setInput("");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/chat", {
+      const response = await fetch("http://72.60.31.189:80/chat", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({
